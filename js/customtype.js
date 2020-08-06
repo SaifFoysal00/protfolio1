d7e11830048649ead6def50df791832daccd7bb7
Saif Foysal <mdsaiffoysal@gmail.com>
2020-08-06 09:25:21 +0600

    html,css,js

---
+++ js/customtype.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".type", {
+    strings:[
+    "ui designer",
+    "fontent developer",
+    "backend developer",
+    "graphics designer",
+    "social media spacelist"
+
+    ],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true,
+});
